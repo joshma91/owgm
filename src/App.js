9f26dd0c90a59fba69de176a2e5fe4532fa8a46e
Joshma91 <josh.ma91@gmail.com>
2018-09-08 15:02:35 -0700

    add firm stat charts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { BarChart, XAxis, YAxis, Bar, Tooltip, Label } from "recharts";
-import { Segment, Header, Tab, Image } from "semantic-ui-react";
+import { Segment, Header, Tab, Image, Grid } from "semantic-ui-react";
 import Particles from "react-particles-js";
 import particlesConfig from "./json/particlesConfig.json";
 import Layout from "./components/Layout";
@@ -12,7 +12,12 @@ import PCT from "./json/PCT.json";
 import OfficeAction from "./json/OfficeAction.json";
 
 class App extends Component {
-  state = { lawyers: null, granted: null, grantedByYear: null };
+  state = {
+    lawyers: null,
+    granted: null,
+    grantedByYear: null,
+    appliedByYear: null
+  };
 
   componentDidMount = async () => {
     this.getLawyers();
@@ -34,32 +39,30 @@ class App extends Component {
 
   getGrantedByYear = () => {
     const grantedFilter = Allowed.filter(x => x.Status == "Granted");
-    const grantedByYearObj = grantedFilter.reduce((acc, curr) => {
-      const grantDate = new Date(curr.DueDate);
-      const year = grantDate.getFullYear();
+    const grantedByYearObj = this.totalByYear(grantedFilter, "DueDate");
 
-      if (typeof acc[year] == "undefined") {
-        acc[year] = 1;
-      } else {
-        acc[year] += 1;
-      }
-      return acc;
-    }, {});
-
-    console.log(grantedByYearObj)
+    console.log(grantedByYearObj);
     const grantedByYear = Object.keys(grantedByYearObj).map(year => {
-      const num = grantedByYearObj[year]
-      return {year, num}
-    })
-    console.log(grantedByYear)
-    
+      const num = grantedByYearObj[year];
+      return { year, num };
+    });
+    console.log(grantedByYear);
+
     this.setState({ grantedByYear });
   };
 
   getAppliedByYear = () => {
-    const appliedByYearObj = this.totalByYear(NonPCT, "FilDate")
-    console.log(appliedByYearObj)
-  }
+    const nonPCTAppliedByYearObj = this.totalByYear(NonPCT, "FilDate");
+    const PCTAppliedByYearObj = this.totalByYear(PCT, "DateTaken");
+
+    const appliedByYear = Object.keys(PCTAppliedByYearObj).map(year => {
+      const numPCT = PCTAppliedByYearObj[year];
+      const numNonPCT = nonPCTAppliedByYearObj[year];
+      return { year, numPCT, numNonPCT };
+    });
+
+    this.setState({ appliedByYear });
+  };
 
   totalByYear = (data, dateField) => {
     const byYear = data.reduce((acc, curr) => {
@@ -73,11 +76,11 @@ class App extends Component {
       }
       return acc;
     }, {});
-    return byYear
-  }
+    return byYear;
+  };
 
   render() {
-    const { lawyers, granted, grantedByYear } = this.state;
+    const { lawyers, granted, grantedByYear, appliedByYear } = this.state;
     const background =
       "https://patentable.com/wp-content/uploads/2016/10/ow_logo_header.png";
     const panes = [
@@ -85,20 +88,58 @@ class App extends Component {
         menuItem: "Firm Statistics",
         render: () => (
           <TabPane>
-            <p style={{float:"left", fontSize:"large"}}>Number of Granted PPH Applications</p>
-            <BarChart width={600} height={300} data={grantedByYear} margin={{top: 50, right: 30, left: 20, bottom: 5}}>>
-              <Label value="Number of Granted PPH Applications" />
-              <XAxis dataKey="year" >
-              </XAxis>
-              <YAxis />
-              <Tooltip/>
-              <Bar
-                type="monotone"
-                dataKey="num"
-                barSize={30}
-                fill="#8884d8"
-              />
-            </BarChart>
+            <Grid columns={2} divided>
+              <Grid.Row>
+                <Grid.Column>
+                  <p style={{ float: "left", fontSize: "large" }}>
+                    Number of Granted PPH Applications
+                  </p>
+                  <BarChart
+                    width={400}
+                    height={300}
+                    data={grantedByYear}
+                    margin={{ top: 50, right: 10, left: 10, bottom: 5 }}
+                  >
+                    <XAxis dataKey="year" />
+                    <YAxis />
+                    <Tooltip />
+                    <Bar
+                      type="monotone"
+                      dataKey="num"
+                      barSize={30}
+                      fill="#8884d8"
+                    />
+                  </BarChart>
+                </Grid.Column>
+                <Grid.Column>
+                  <p style={{ float: "left", fontSize: "large" }}>
+                    Number of PPH Applications
+                  </p>
+                  <BarChart
+                    width={400}
+                    height={300}
+                    data={appliedByYear}
+                    margin={{ top: 50, right: 10, left: 10, bottom: 5 }}
+                  >
+                    <XAxis dataKey="year" />
+                    <YAxis />
+                    <Tooltip />
+                    <Bar
+                      type="monotone"
+                      dataKey="numPCT"
+                      barSize={30}
+                      fill="#8884d8"
+                    />
+                    <Bar
+                      type="monotone"
+                      dataKey="numNonPCT"
+                      barSize={30}
+                      fill="#82ca9d"
+                    />
+                  </BarChart>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
           </TabPane>
         )
       },
@@ -122,7 +163,6 @@ class App extends Component {
 
           <Segment>
             <Header as="h2">Basic Stats</Header>
-            <p>Lawyers: {lawyers}</p>
             <p>Number of Allowed PPH Applications: {granted}</p>
           </Segment>
 
