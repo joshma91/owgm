f824657fa872a07efd72af16c31933994826a417
Joshma91 <josh.ma91@gmail.com>
2018-09-21 00:26:50 -0700

    add time to first OA chart

---
+++ src/components/OAStats.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Grid } from "semantic-ui-react";
+import { BarChart, XAxis, YAxis, Bar, Tooltip, Label } from "recharts";
+
+export default class OAStats extends React.Component {
+  render() {
+
+    const {oaTimeByYear} = this.props
+
+    return (  
+      <Grid columns={2} divided>
+        <Grid.Row>
+          <Grid.Column>
+            <p style={{ float: "left", fontSize: "large" }}>
+              Average Time to First Office Action
+            </p>
+            <BarChart
+              width={400}
+              height={300}
+              data={oaTimeByYear}
+              margin={{ top: 50, right: 10, left: 10, bottom: 5 }}
+            >
+              <XAxis dataKey="year" />
+              <YAxis />
+              <Tooltip />
+              <Bar type="monotone" dataKey="months" barSize={30} fill="#8884d8" />
+            </BarChart>
+          </Grid.Column>
+          <Grid.Column>
+            <p style={{ float: "left", fontSize: "large" }}>
+              Applications Receiving Immediate Grant
+            </p>
+           
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    );
+  }
+}
